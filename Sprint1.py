bf11d4639d1e90a711a2c6ff884894b4c5708f9a
Kacper <54666998+dabkac@users.noreply.github.com>
2022-05-02 00:09:05 +0200

    Add files via upload

--- Sprint1.py
+++ Sprint1.py
@@ -1,10 +1,36 @@
 oceny = open("C:\\Users\\kdabek\\Desktop\\UpSkill - Python\\dane_wejsciowe\\sprint1\\grades1.txt", 'r').read()
 print(oceny)
-Cezary = ((3+5+1+3+1+1+3+4+6)/9)
-print(float(Cezary))
-Dionizy =((6+3+6+5.5+5+3.5+4)/7)
-print(float(Dionizy))
-Barnaba = ((5+4+5.5+4+6+3.5+1)/7)
-print(float(Barnaba))
-Albert = ((3+3+2+4+6+5+2)/7)
-print(float(Albert))
+a = 3
+b = 5
+c = 1
+d = 3
+e = 1
+f = 1
+g = 3
+h = 4
+i = 6
+print("Cezary", (a+b+c+d+e+f+g+h+i)/9 )
+a1 = 6
+b1 = 3
+c1 = 6
+d1 = 5.5
+e1 = 5
+f1 = 3.5
+g1 = 4
+print("Dionizy", (a1+b1+c1+d1+e1+f1+g1)/7)
+a2 = 5
+b2 = 4
+c2 = 5.5
+d2 = 4
+e2 = 6
+f2 = 3.5
+g2 = 1
+print("Barnaba", (a2+b2+c2+d2+e2+f2+g2)/7)
+a3 = 3
+b3 = 3
+c3 = 2
+d3 = 4
+e3 = 6
+f3 = 5
+g3 = 2
+print("Albert", (a3+b3+c3+d3+e3+f3+g3)/7)
