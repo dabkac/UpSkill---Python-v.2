a63056039ffd1106344f80c0755eca962fac678e
Kacper <54666998+dabkac@users.noreply.github.com>
2022-05-16 00:03:07 +0200

    Revert "With.Open"

--- Sprint 1 - with open.py
+++
@@ -1,7 +0,0 @@
-with open("C:\\Users\\kdabek\\Desktop\\UpSkill - Python\\dane_wejsciowe\\sprint1\\grades1.txt", "r") as file:
-    for line in file:
-        print(line)
-        linesplited = line.split()
-        
-        # line = [float(x) for x in line.split()]
-        # print(line, line)
