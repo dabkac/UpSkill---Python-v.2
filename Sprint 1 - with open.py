c3f854705df9e9c2a59776ed4f6fc76b4d0358c8
Kacper <54666998+dabkac@users.noreply.github.com>
2022-06-07 15:03:37 +0200

    Add files via upload

--- Sprint 1 - with open.py
+++ Sprint 1 - with open.py
@@ -1,26 +1,46 @@
+from os import makedirs
 import string
+from time import time
 from typing import List
-
+import datetime
+nowtime = datetime.datetime.now()
+print(nowtime.strftime("%H:%M %d.%m.%Y"))
 
 with open("C:\\Users\\kdabek\\Desktop\\UpSkill - Python\\dane_wejsciowe\\sprint1\\grades1.txt", "r") as file:
     for line in file:
         linesplited = line.strip().split(',')
         name = linesplited[0]
-        # print(str(name))
-        liczby = linesplited[1:]
-        all_oceny = []
-        for x in liczby: 
-            ocena = float(x)
-            all_oceny.append(ocena)
-        # print(all_oceny)
-        dlugosc = len(all_oceny)
-        suma = sum(all_oceny)
-        avg = suma/dlugosc
-        print(name, end=" ")
-        print(round(avg, 2))
+        numbers = linesplited[1:]
+        all_marks = []
+        for x in numbers: 
+            mark = float(x)
+            all_marks.append(mark)
+        lenght = len(all_marks)
+        all = sum(all_marks)
+        avg = all/lenght
+        int_avg= int(avg)
+        dictionary = {name : round(avg, 2)} 
+        sorted_list_name = sorted(dictionary)
+        #sorted_list_marks = sorted(int_avg)
+        
 
-            # przypisanie sredniej do ucznia 
+        #sorted_list_marks = sorted(round(avg, 2))
+        print(dictionary)
+        print(sorted_list_name)
+       # print(sorted_list_marks)
+        
+        
+        
+        
+    
+# da się z jednego printAa
+
+            # słowniki 
+            # przypisanie sredniej do ucznia - przypisanie do jednej zmiennej, dać mozliwość wyboru , 
+            # sortowanie po im i sredniej
+            # w jaki sposób urzytkownik bedzie sortował
             # dokumentacja printa , połączenie w jednej lini
+            # 
             
             
             
